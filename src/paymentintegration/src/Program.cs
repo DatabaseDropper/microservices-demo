26f0ded482fbcadf412df13e6242faff75f2a1f1
DatabaseDropper <47637776+DatabaseDropper@users.noreply.github.com>
2023-01-15 12:24:19 +0100

    test

--- src/paymentintegration/src/Program.cs
+++ src/paymentintegration/src/Program.cs
@@ -3,14 +3,25 @@ using System.Net;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
+builder.Services.AddCors(o =>
+{
+    o.AddPolicy("cors", b =>
+    {
+        b.AllowAnyHeader();
+        b.AllowAnyMethod();
+        b.AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
+app.UseCors("cors");
 
 app.MapPost("/payu", async (HttpContext context) =>
 {
     var data = await context.Request.ReadFromJsonAsync<dynamic>();
     Console.WriteLine(data);
     context.Response.StatusCode = (int)HttpStatusCode.OK;
-    return "OK";
+    return "{\"status\": \"ok\"}";
 });
 
 app.UseHttpsRedirection();
