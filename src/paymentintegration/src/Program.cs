9003db37cd97a820dfbe888b36ce32bd8fe61aad
DatabaseDropper <47637776+DatabaseDropper@users.noreply.github.com>
2023-01-15 13:00:34 +0100

    .

--- src/paymentintegration/src/Program.cs
+++ src/paymentintegration/src/Program.cs
@@ -6,18 +6,13 @@ using System.Threading;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
-builder.Services.AddCors(o =>
-{
-    o.AddPolicy("cors", b =>
-    {
-        b.AllowAnyHeader();
-        b.AllowAnyMethod();
-        b.AllowAnyOrigin();
-    });
-});
-
 var app = builder.Build();
-app.UseCors("cors");
+app.UseCors(x => x
+          .AllowAnyMethod()
+          .AllowAnyHeader()
+          .SetIsOriginAllowed(origin => true) // allow any origin
+                                              //.WithOrigins("https://localhost:44351")); // Allow only this origin can also have multiple origins separated with comma
+          .AllowCredentials()); // allow credentials
 
 app.MapGet("/payu", async (HttpContext context) =>
 {
